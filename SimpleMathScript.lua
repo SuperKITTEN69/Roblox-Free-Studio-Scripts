8894a85a6befdf600c6cbf40a95cb40063eee9a5
KITTENTHEIII <112276428+SuperKITTEN69@users.noreply.github.com>
2025-01-02 17:10:38 +0000

    Add files via upload

---
+++ SimpleMathScript.lua
@@ -0,0 +1,9 @@
+local Cats = 10  -- You can change the number and the text after local
+local Dogs = 4   -- You can change the number and the text after local
+
+print("You have ".. Cats + Dogs .." Animals")
+
+
+-- You can use warn or error instead of print but in this case you will not need that
+
+--Made by srrafolas
